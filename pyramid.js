d6d3463dc4e5866131d39cde8d8e4f796e31ef75
Reed Brooks <reedb36@gmail.com>
2021-03-17 14:58:49 -0700

    Implements file parsing

--- pyramid.js
+++ pyramid.js
@@ -14,14 +14,53 @@ const fs = require('fs');
 }
 */
 function parseFile(fileName) {
+  console.log(fileName);
   let problem = {};
+
+  let rawArray = fs.readFileSync(fileName).toString().split(`\n`);
+
+  //get target
+  let targetValue = rawArray[0].replace('Target:', '').replace(' ','');
+  problem.target = parseInt(targetValue);
+
+  //get pyramid
+  problem.pyramid = [];
+
+  for(let i = 1; i < rawArray.length; i++) {
+
+    let rawRowData = rawArray[i].split(`,`);
+    let rowArray = [];
+
+    for(j = 0; j < rawRowData.length; j++) {
+      let rowNumber = parseInt(rawRowData[j].replace(' ', ''));
+      rowArray.push(rowNumber);
+    }
+
+    problem.pyramid.push(rowArray);
+  }
 
   return problem;
 }
 
 //main body
+if(process.argv.length <= 2) {
+  console.log("ERROR: no file(s) specified");
+  return -1;
+}
+
+console.log("Checking solutions for pyramid problems...");
 
 //for each file provided, parse data from file and pass the total/pyramid values to the solvePyramid function.  Print the solution to the console.
 for(let i = 2; i < process.argv.length; i++) {
-  console.log(process.argv[i]);
+  let fileName = process.argv[i];
+  let problem = parseFile(fileName);
+
+  console.log(`Now checking ${fileName}...`);
+  console.log(`Target value is ${problem.target}`);
+  console.log(`Target pyramid is...`);
+  for(let j = 0; j < problem.pyramid.length; j++) {
+    console.log(problem.pyramid[j]);
+  }
+
+  //TBD: pass problem data to solvePyramid, return result
 }
